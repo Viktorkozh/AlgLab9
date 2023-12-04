2d42c0bb8ea4259886a010f8185ccff4393ce9f9
ViktorKozh <viktor.ru253@gmail.com>
2023-12-04 12:36:19 +0300

    Still havent fixed the fourth graph

--- Main.py
+++ Main.py
@@ -10,7 +10,6 @@ from scipy.optimize import curve_fit
 
 amount_of_dots = 1000  # Количество точек
 aod = (amount_of_dots + 1) * 10
-a = []
 worst_time = {}
 median_time = {}
 graph_stuff = [i for i in range(10, 10010, 10)]
@@ -46,28 +45,49 @@ def logarithmic_model(x, a, b):
     return a * np.log(x) + b
 
 
-def lsm(name, time, graph_num):
-    x_data = np.array(graph_stuff)
-    y_data = np.array(list(time.values()))
-
-    params, _ = curve_fit(logarithmic_model, x_data, y_data)
-
-    a_fit, b_fit = params
-    print(
-        f"Коэффициенты уравнения ({name}): a = {a_fit}, b = {b_fit}")
-
-    x_fit = np.linspace(min(x_data), max(x_data), 100)
-    y_fit = logarithmic_model(x_fit, *params)
-
+def lsm(name, time, graph_num, log):
     plt.figure(graph_num).set_figwidth(8)
     plt.title(
         f"Зависимость времени поиска элемента от размера массива\n({name})")
-    plt.plot(x_fit, y_fit, "r-", label="Quadratic Fit")
-    plt.scatter(graph_stuff, time.values(), s=5, c="orange")
     plt.xlabel(xlabel)
     plt.ylabel(ylabel)
     plt.tight_layout()
     plt.grid(False)
+
+    if log == 1:
+        x_data = np.array(graph_stuff)
+        y_data = np.array(list(time.values()))
+
+        params, _ = curve_fit(logarithmic_model, x_data, y_data)
+
+        a_fit, b_fit = params
+        print(
+            f"Коэффициенты уравнения ({name}): a = {a_fit}, b = {b_fit}")
+
+        x_fit = np.linspace(min(x_data), max(x_data), 100)
+        y_fit = logarithmic_model(x_fit, *params)
+
+        plt.plot(x_fit, y_fit, "r-", label="Quadratic Fit")
+    else:
+        A = np.vstack([graph_stuff, np.ones(len(graph_stuff))]).T
+        alpha = np.dot(
+            (np.dot(np.linalg.inv(np.dot(A.T, A)), A.T)
+             ), np.array(list(time.values()))
+        )
+        plt.plot(graph_stuff, alpha[0] *
+                 np.array(list(graph_stuff)) + alpha[1], "r")
+
+        formatted_alpha = [format(a, ".10f") for a in alpha]
+        print(
+            f"Коэффициенты прямой {name}: a =",
+            formatted_alpha[0],
+            "b =",
+            formatted_alpha[1],
+        )
+        print(f"Корреляция {name}:", np.corrcoef(
+            graph_stuff, list(time.values()))[0, 1])
+
+    plt.scatter(graph_stuff, time.values(), s=5, c="orange")
 
 
 if __name__ == '__main__':
@@ -76,7 +96,7 @@ if __name__ == '__main__':
         worst_time[i] = (timeit.timeit(
             lambda: bin_search(a, 10000000), number=100)) / 100
 
-    lsm("Худший случай", worst_time, 1)
+    lsm("Худший случай", worst_time, 1, True)
 
     for i in range(10, aod, 10):
         a = fill_list(i)
@@ -84,15 +104,14 @@ if __name__ == '__main__':
         median_time[i] = timeit.timeit(
             lambda: bin_search(a, a[t]), number=100) / 100
 
-    lsm("Средний случай", median_time, 2)
-
+    lsm("Средний случай", median_time, 2, True)
 
     for i in range(10, aod, 10):
         a = fill_list(i)
         worst_time[i] = (timeit.timeit(
             lambda: search(a, 10000000), number=100)) / 100
 
-    lsm("Худший случай", worst_time, 3)
+    lsm("Худший случай", worst_time, 3, False)
 
     for i in range(10, aod, 10):
         a = fill_list(i)
@@ -100,6 +119,6 @@ if __name__ == '__main__':
         median_time[i] = timeit.timeit(
             lambda: search(a, a[t]), number=100) / 100
 
-    lsm("Средний случай", median_time, 4)
+    lsm("Средний случай", median_time, 4, False)
 
     plt.show()
